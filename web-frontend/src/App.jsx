53c9703fd302564c5572fa2673c76e39441981ea
unknown <karanx11.72898@gmail.com>
2026-03-09 23:33:43 +0530

    fix routing with HashRouter for Render

--- web-frontend/src/App.jsx
+++ web-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useRef } from "react";
 
 import KokkieBot from "./components/KokkieBot";
@@ -80,7 +80,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
 
         {/* ===== PUBLIC ROUTES ===== */}
@@ -173,7 +173,7 @@ function App() {
 
       {/* ===== GLOBAL FLOATING COMPONENT ===== */}
       <KokkieBot />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
