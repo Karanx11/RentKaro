29c002eafe5f18361d91e8b03d8e7164bd0a7b5a
unknown <karanx11.72898@gmail.com>
2026-04-02 23:04:13 +0530

    Updating Render Issue

--- web-backend/controllers/authController.js
+++ web-backend/controllers/authController.js
@@ -103,8 +103,8 @@ export const loginUser = async (req, res) => {
 
   res.cookie("refreshToken", refreshToken, {
   httpOnly: true,
-  secure: false, // true in production (HTTPS)
-  sameSite: "lax", 
+  secure: true,        
+  sameSite: "none",    
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
